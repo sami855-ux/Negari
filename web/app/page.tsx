04050b604ed5c185f92fbcba77cfc79ea3057776
sami855-ux <samitale56@outlook.com>
2025-06-20 23:12:58 +0300

    Add FeaturesSection, HowItWorks, and MobileAppPreview components to Home page

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,12 +1,18 @@
+import { FeaturesSection } from "@/components/landing/Features"
 import Footer from "@/components/landing/Footer"
 import Header from "@/components/landing/Header"
 import Hero from "@/components/landing/Hero"
+import HowItWorks from "@/components/landing/HowItWorks"
+import MobileAppPreview from "@/components/landing/MobilePreview"
 
 /*******  3beb5042-7fbe-4562-bf0e-fb32d202c5b4  *******/ const Home = () => {
   return (
     <>
       <Header />
       <Hero />
+      <FeaturesSection />
+      <HowItWorks />
+      <MobileAppPreview />
       <Footer />
     </>
   )
