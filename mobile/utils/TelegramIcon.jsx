80aa494002b7e0c81cf5487b89c80603cdca5366
sami855-ux <samitale56@outlook.com>
2025-06-24 00:06:52 +0300

    telegram icon is added

--- mobile/utils/TelegramIcon.jsx
+++ mobile/utils/TelegramIcon.jsx
@@ -1,8 +1,8 @@
-import { Circle, Path } from "react-native-svg"
+import Svg, { Circle, Path } from "react-native-svg"
 
 export default function TelegramIcon() {
   return (
-    <svg viewBox="0 0 240 240" className="w-5 h-5">
+    <Svg viewBox="0 0 240 240" width={20} height={20}>
       <Circle cx="120" cy="120" r="120" fill="#37aee2" />
       <Path
         fill="#c8daea"
@@ -16,6 +16,6 @@ export default function TelegramIcon() {
         fill="#fff"
         d="M94 152l5-63c1-6 9-8 12-2l14 29 40-36c5-4 11 2 7 7l-54 53-21 8c-4 2-4 0-3-3z"
       />
-    </svg>
+    </Svg>
   )
 }
