5bfac9b2366fcc3c6e53e790d7e62c14aa42dbee
sami855-ux <samitale56@outlook.com>
2025-06-24 12:26:09 +0300

    feat: add request validation middleware and user input validators for registration and login

---
+++ server/src/routes/auth.route.js
@@ -0,0 +1,39 @@
+import express from "express"
+
+import { registerUser, loginUser } from "../controllers/authApp.controller.js"
+import { registerValidator, loginValidator } from "../utils/authValidator.js"
+import validateRequest from "../middleware/validateRequest.js"
+import {
+  loginUserWeb,
+  logoutUserWeb,
+  registerUserWeb,
+} from "../controllers/authWeb.controller.js"
+import loginLimiter from "../utils/LoginLimter.js"
+
+const router = express.Router()
+
+router.post("/app/register", registerValidator, validateRequest, registerUser)
+router.post(
+  "/app/login",
+  loginLimiter,
+  loginValidator,
+  validateRequest,
+  loginUser
+)
+
+router.post(
+  "/web/register",
+  registerValidator,
+  validateRequest,
+  registerUserWeb
+)
+router.post(
+  "/web/login",
+  loginLimiter,
+  loginValidator,
+  validateRequest,
+  loginUserWeb
+)
+router.post("/web/logout", logoutUserWeb)
+
+export default router
