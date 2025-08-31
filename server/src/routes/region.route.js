8eb1e259ce26d3c7f446108792cacdd35895982f
sami855-ux <samitale56@outlook.com>
2025-08-31 14:25:52 +0300

    feat: add route to get region by officer ID

--- server/src/routes/region.route.js
+++ server/src/routes/region.route.js
@@ -1,9 +1,15 @@
 import express from "express"
-import { getAllRegions } from "../controllers/region.controller.js"
+import {
+  getAllRegions,
+  getRegionByOfficer,
+} from "../controllers/region.controller.js"
 
 const router = express.Router()
 
 //Get all the regions
 router.get("/", getAllRegions)
+
+//Get a region for the officer
+router.get("/:officerId", getRegionByOfficer)
 
 export default router
