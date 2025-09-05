eb8a422fb9435579a8c74115d71353d578b83b99
sami855-ux <samitale56@outlook.com>
2025-09-05 14:51:45 +0300

    notification slice is added to the store

--- mobile/store/index.js
+++ mobile/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
 
 import authReducer from "./slices/auth"
+import notificationReducer from "./slices/notification"
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    notification: notificationReducer,
   },
 })
