8ac1bbda46993f243149f92b025c7904d6b04d26
sami855-ux <samitale56@outlook.com>
2025-09-09 23:02:05 +0300

    update -3

--- web/services/auth.js
+++ web/services/auth.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://negari.onrender.com/api",
   withCredentials: true,
 })
 
